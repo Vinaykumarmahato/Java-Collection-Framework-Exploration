803892bd6fccbe44d351a6dfdf6882942026854e
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-12 16:22:55 +0530

    Updated

---
+++ 📁 ArrayList/ArrayListSolution2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class ArrayListSolution2 {
+    
+    public static void main(String[] args) {
+
+        ArrayList al1=new ArrayList();
+
+        al1.add(1000);
+        al1.add(2000);
+        al1.add(3000);
+        al1.add(4000);
+        System.out.println(al1);
+        System.out.println("----------------------------------");
+
+        // Contains() used for check the values whether given value is in arraylist or not
+
+        al1.contains(4000);
+        System.out.println(al1);
+
+
+
+    }
+}
