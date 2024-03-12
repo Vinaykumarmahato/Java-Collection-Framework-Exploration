9c38893553130c2398e21a88beef6bb2ba94e440
ADV Indian coder <vinaykumar860964@gmail.com>
2024-03-12 21:13:24 +0530

    Updated

---
+++ 📁 ArrayList/ArrayListSolution4.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListSolution4 {
+    
+    public static void main(String[] args) {
+        
+        List al=new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        System.out.println(al);
+    }
+}
