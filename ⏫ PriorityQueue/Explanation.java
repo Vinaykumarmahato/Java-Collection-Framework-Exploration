9ababcf113e41b42862748914f75887a3dbf9766
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-13 16:41:59 +0530

    Updated

--- ⏫ PriorityQueue/Explanation.java
+++ ⏫ PriorityQueue/Explanation.java
@@ -3,7 +3,7 @@ import java.util.PriorityQueue; // Importing the PriorityQueue class from the ja
 public class Explanation { // Declaring a public class named Explanation
 
     public static void main(String[] args) { // Declaring the main method
-        
+
         PriorityQueue pq = new PriorityQueue(); // Creating a new instance of PriorityQueue named pq
 
         // Adding elements to the PriorityQueue
@@ -23,12 +23,14 @@ public class Explanation { // Declaring a public class named Explanation
         // Printing the contents of the PriorityQueue pq
 
         System.out.println("--------------------");
-// Not Allow Here Heterogenous data.
+        // Not Allow Here Heterogenous data.
         // pq.add("Vinay Kumar");
         // System.out.println(pq);
-        
-        // Here the data will not be added in order and also won't be added in an unsorted way
-        // It follows the min-heap Data Structure, which is why it is happening like this.
+
+        // Here the data will not be added in order and also won't be added in an
+        // unsorted way
+        // It follows the min-heap Data Structure, which is why it is happening like
+        // this.
 
     }
 }
