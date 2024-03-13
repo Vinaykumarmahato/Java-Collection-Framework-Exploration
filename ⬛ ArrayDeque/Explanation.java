65e064857cca7592530d7433d47620d5f786aba0
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-13 15:41:58 +0530

    Updated

---
+++ ⬛ ArrayDeque/Explanation.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque; // Importing the ArrayDeque class from the java.util package
+
+public class Explanation { // Declaring a public class named Explanation
+
+    public static void main(String[] args) { // Declaring the main method
+
+        ArrayDeque ad1 = new ArrayDeque(); // Creating a new instance of ArrayDeque named ad1
+        ad1.add(100); // Adding the integer 100 to the ArrayDeque ad1
+        ad1.add(200); // Adding the integer 200 to the ArrayDeque ad1
+
+        System.out.println(ad1); // Printing the contents of the ArrayDeque ad1
+    }
+}
