b2acea6ce20f1cdecd6b13ad4cdbf077d0523485
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-14 13:41:27 +0530

    Updated

--- 🌲 TreeSet/Explanation.java
+++ 🌲 TreeSet/Explanation.java
@@ -4,8 +4,19 @@ public class Explanation {
     
     public static void main(String[] args) {
         
-        TreeSet t=new TreeSet();
-
+        TreeSet t = new TreeSet<>();
+        t.add(100);
+        t.add(23);
+        t.add(9);
+        t.add(90);
+        t.add(877);
+        t.add(45);
         
+        System.out.println(t);
+        // It stores the data in a Binary Search Tree way.
+        // The data will be in sorted order.
+
+        // output:
+        // [9, 23, 45, 90, 100, 877]
     }
 }
