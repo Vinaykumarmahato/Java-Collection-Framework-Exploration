67d5be2d045182027623259b432d02301441a170
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-15 12:34:57 +0530

    Updated

--- 🔵 HashSet and LinkedHashSet/ExplanationOFBothLinkedHsahSetAndHashSet.java
+++ 🔵 HashSet and LinkedHashSet/ExplanationOFBothLinkedHsahSetAndHashSet.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 public class ExplanationOFBothLinkedHsahSetAndHashSet {
 
     public static void main(String[] args) {
-        
+
         // HashSet: it will not preserve the order of insertion
         // Introduced in Java 1.2
 
@@ -28,3 +28,25 @@ public class ExplanationOFBothLinkedHsahSetAndHashSet {
 
     }
 }
+
+// Unordered Collection: HashSet does not maintain any order of elements.
+// Unique Elements: HashSet does not allow duplicate elements.
+// Performance: Offers constant-time performance for basic operations (add,
+// remove, contains) assuming a good hash function.
+// Backed by HashMap: Internally uses a HashMap to store elements.
+// Iteration Order: The iteration order of a HashSet is unpredictable and may
+// change over time due to resizing and rehashing operations.
+// Introduced in Java Version: HashSet was introduced in Java 1.2.
+// LinkedHashSet:
+
+// Maintains Insertion Order: LinkedHashSet maintains the insertion order of
+// elements, i.e., the order in which elements were inserted into the set.
+// Unique Elements: Like HashSet, LinkedHashSet also does not allow duplicate
+// elements.
+// Performance: Offers constant-time performance for basic operations (add,
+// remove, contains) assuming a good hash function.
+// Backed by LinkedHashMap: Internally uses a LinkedHashMap to store elements,
+// maintaining a doubly linked list to maintain insertion order.
+// Iteration Order: The iteration order of a LinkedHashSet is the order in which
+// elements were inserted into the set.
+// Introduced in Java Version: LinkedHashSet was introduced in Java 1.4.
