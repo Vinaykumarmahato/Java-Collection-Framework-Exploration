0b3bd1a8f9bc4625d6f55b32a2758fa25ee9bef1
Vinay kumar <vinaykumar860964@gmail.com>
2024-03-18 14:34:17 +0530

    Updated

---
+++ 🗼 Map Hierarchy/Explanation.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class Explanation {
+    
+    public static void main(String[] args) {
+        
+
+        HashMap hm=new HashMap();
+        Hashtable ht=new Hashtable();
+        LinkedHashMap lhm=new LinkedHashMap();
+        TreeMap tm=new TreeMap();
+
+        
+
+
+
+    }
+}
