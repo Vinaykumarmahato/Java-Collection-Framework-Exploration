79b1331270d6598b0f986a3dc4eccbf4eb0147fc
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2025-01-20 21:53:28 +0530

    Update Explanation.java

--- ⬛ ArrayDeque/Explanation.java
+++ ⬛ ArrayDeque/Explanation.java
@@ -7,6 +7,7 @@ public class Explanation { // Declaring a public class named Explanation
         ArrayDeque ad1 = new ArrayDeque(); // Creating a new instance of ArrayDeque named ad1
         
 
+        
         // Adding elements to the ArrayDeque
         ad1.add(100); // Adding the integer 100 to the ArrayDeque ad1
         ad1.add(200); // Adding the integer 200 to the ArrayDeque ad1
